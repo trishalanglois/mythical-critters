bb5248695573edb0b07735511fa24df690213985
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-08-25 15:04:59 -0600

    Go through vampire JS and tests

--- exercises/vampire.js
+++ exercises/vampire.js
@@ -2,3 +2,16 @@
 //2.ASK YOURSELF IF IT'S GOING TO FAIL AND WHY
 //3.RUN TEST
 //4.MAKE IT PASS
+
+class Vampire  {
+  constructor(name, pet) {
+    this.name = name;
+    this.pet = pet || 'bat';
+    this.thirsty = true;
+  }
+  drink() {
+    this.thirsty = false;
+  }
+}
+
+module.exports = Vampire;
